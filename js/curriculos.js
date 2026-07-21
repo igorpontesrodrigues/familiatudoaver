1a857a4f55afdf3cc0aace91f0ebfe5b27444634
caratininga <igorprcaratininga@gmail.com>
2026-07-21 10:47:54 -0300

    fix: restore CNH combinations AB, AC, AD, AE

--- js/curriculos.js
+++ js/curriculos.js
@@ -103,6 +103,10 @@ function initCurriculos() {
                         <option value="C">C</option>
                         <option value="D">D</option>
                         <option value="E">E</option>
+                        <option value="AB">AB</option>
+                        <option value="AC">AC</option>
+                        <option value="AD">AD</option>
+                        <option value="AE">AE</option>
                     </select>
                 </div>
                 <div class="md:col-span-4">
